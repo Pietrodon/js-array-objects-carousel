2340910a0ea0f0b9a8b7fc82df6759ca6e1fddf1
Pietrodon <donvitopietro00@gmail.com>
2022-02-24 18:00:10 +0100

    creazione struttura statica

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Dati tre array contenenti:
 // - una lista ordinata di 5 immagini,
-// - una lista ordinata dei relativi 5 luoghi e
-// - una lista di 5 news, creare un carosello come nella foto allegata.
+// - una lista ordinata dei relativi 5 luoghi 
+// - una lista di 5 news,
+//  creare un carosello come nella foto allegata.
