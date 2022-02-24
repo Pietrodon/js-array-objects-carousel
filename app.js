d54536ebd5c7f4fbcea5643cfa207d696c05aa07
Pietrodon <donvitopietro00@gmail.com>
2022-02-24 17:25:04 +0100

    html di partenza e collegamento js e css

---
+++ app.js
@@ -0,0 +1,4 @@
+// Dati tre array contenenti:
+// - una lista ordinata di 5 immagini,
+// - una lista ordinata dei relativi 5 luoghi e
+// - una lista di 5 news, creare un carosello come nella foto allegata.
